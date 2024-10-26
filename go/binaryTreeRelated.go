d37d78f4200a3261c70e6068ea87d73d510fa416
liang <liangyens2012@gmail.com>
2024-10-26 10:26:44 -0300

    fixed go package

--- go/binaryTreeRelated.go
+++ go/binaryTreeRelated.go
@@ -1,20 +1,29 @@
+package main
+
 func findTilt(root *TreeNode) int {
-    if root == nil {
-        return 0
-    }
-    return abs(findSum(root.Left)-findSum(root.Right)) + findTilt(root.Left) + findTilt(root.Right)
+	if root == nil {
+		return 0
+	}
+	return abs(findSum(root.Left)-findSum(root.Right)) + findTilt(root.Left) + findTilt(root.Right)
 }
 
 func findSum(node *TreeNode) int {
-    if node == nil {
-        return 0
-    }
-    return node.Val + findSum(node.Left) + findSum(node.Right)
+	if node == nil {
+		return 0
+	}
+	return node.Val + findSum(node.Left) + findSum(node.Right)
 }
 
 func abs(num int) int {
-    if num < 0 {
-        return 0 - num
-    }
-    return num
+	if num < 0 {
+		return 0 - num
+	}
+	return num
+}
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
